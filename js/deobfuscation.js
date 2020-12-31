8edea3ba38c03f4937242b9a1be3e37d40318b37
sunDalik <dali1999@mail.ru>
2020-12-31 16:23:31 +0300

    Add forced identation

--- js/deobfuscation.js
+++ js/deobfuscation.js
@@ -1,8 +1,21 @@
 const varName = "[A-Za-z][A-Za-z0-9_\-]*";
+
 const functionRegex = new RegExp(`^[ \\t]*(Public|Private|)[ \\t]*(Sub|Function)[ \\t]+(?<functionName>${varName})[ \\t]*\(.*\)[ \\t]*$`);
 const forRegex = new RegExp(`^[ \\t]*For[ \\t]+(?<iteratorVariable>${varName})[ \\t]*=\\s*$`);
 
 const variableDeclarationRegex = new RegExp(`(^[ \\t]*(Set|Dim)[ \\t]+(?<variableName>${varName}).*?$)|(^[ \\t]*(?<variableName2>${varName})([ \\t]*\(.+?\))?[ \\t]*=.*?$)`);
+
+const blocks = [{start: "Sub", end: "End"},
+    {start: "Function", end: "End"},
+    {start: "Do", end: "Loop"},
+    {start: "For", end: "Next"},
+    {start: "If", end: "End"},
+    {start: "Select", end: "End"},
+    {start: "SyncLock", end: "End"},
+    {start: "Try", end: "End"},
+    {start: "While", end: "End"},
+    {start: "With", end: "End"}
+];
 
 const settings = {
     renameVariables: true,
@@ -11,9 +24,53 @@ const settings = {
 
 export function deobfuscateCode(code) {
     let deobfuscatedCode = code;
+    deobfuscatedCode = shrinkSpaces(deobfuscatedCode);
     if (settings.removeDeadCode) deobfuscatedCode = removeDeadCode(deobfuscatedCode);
     if (settings.renameVariables) deobfuscatedCode = renameVariables(deobfuscatedCode);
+    deobfuscatedCode = identCode(deobfuscatedCode);
     return deobfuscatedCode;
+}
+
+function shrinkSpaces(code) {
+    const codeLines = code.split("\n");
+    for (let i = 0; i < codeLines.length; i++) {
+        codeLines[i] = codeLines[i].trim();
+    }
+    code = codeLines.join("\n");
+
+    //code = code.replace(/\t/g, ' ');
+    //code = code.replace(/  +/g, ' ');
+    return code;
+}
+
+function identCode(code) {
+    const blockIdent = "    ";
+    let blockStack = [];
+
+    const codeLines = code.split("\n");
+    for (let i = 0; i < codeLines.length; i++) {
+        let lvlMod = 0;
+        codeLines[i] = codeLines[i].trim();
+        const matchResult = codeLines[i].match(new RegExp("^\\s*(?<firstWord>\\b[A-Za-z]+?\\b).*?$"));
+        if (matchResult) {
+            const firstWord = matchResult.groups.firstWord;
+            const blockStart = blocks.find(b => b.start === firstWord);
+            if (blockStart) {
+                blockStack.push(blockStart);
+                lvlMod = -1;
+            } else if (blockStack.length > 0 && blockStack[blockStack.length - 1].end === firstWord) {
+                const poppedBlock = blockStack.pop();
+                if (poppedBlock.start === "Sub" || poppedBlock.start === "Function") {
+                    codeLines.splice(i + 1, 0, "");
+                }
+            }
+        }
+        for (let lvl = 0; lvl < blockStack.length + lvlMod; lvl++) {
+            codeLines[i] = blockIdent + codeLines[i];
+        }
+    }
+    code = codeLines.join("\n");
+    return code;
 }
 
 function renameVariables(code) {
